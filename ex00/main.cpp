3192015da66c4c0534b58dbd75807aa6b39e7b1d
Davit Grigoryan <dgrigor2@c2r11s2.42yerevan.am>
2026-06-08 17:00:08 +0400

    some tests added

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -5,42 +5,33 @@
 
 int main()
 {
-	{
-		Animal* zoo[10];
-		for (int i = 0; i < 10; i++)
-		{
-			if (i < 5)
-			{
-				zoo[i] = new Dog();
-			}
-			else
-			{
-				zoo[i] = new Cat();
-			}
-		}
-		for (int i = 0; i < 10; i++)
-		{
-			(*zoo[i]).makeSound();
-		}
-		for (int i = 0; i < 10; i++)
-		{
-			delete zoo[i];
-		}
-	}
-	{
-		const Animal* meta = new Animal();
-		const Animal* j = new Dog();
-		const Animal* i = new Cat();
-		std::cout << j->getType() << " " << std::endl;
-		std::cout << i->getType() << " " << std::endl;
-		i->makeSound(); //will output the cat sound!
-		j->makeSound();
-		meta->makeSound();
-		delete meta;
-		delete j;
-		delete i;
-		return 0;
-	}
+	// {
+	// 	Animal anim;
+	// 	Cat cat;
+	// 	Cat copyCat(cat);
+	// 	Dog hafo;
+	// 	std::cout << cat.getType() << std::endl;
+	// 	std::cout << copyCat.getType() << std::endl;
+	// 	std::cout << hafo.getType() << std::endl;
+	// 	std::cout << anim.getType() << std::endl;
+	// 	cat.makeSound();
+	// 	copyCat.makeSound();
+	// 	hafo.makeSound();
+	// 	anim.makeSound();
+	// }
+	// {
+	// 	const Animal* meta = new Animal();
+	// 	const Animal* j = new Dog();
+	// 	const Animal* i = new Cat();
+	// 	std::cout << j->getType() << " " << std::endl;
+	// 	std::cout << i->getType() << " " << std::endl;
+	// 	i->makeSound(); //will output the cat sound!
+	// 	j->makeSound();
+	// 	meta->makeSound();
+	// 	delete meta;
+	// 	delete j;
+	// 	delete i;
+	// }
 	{
 		const WrongAnimal* meta = new WrongAnimal();
 		const WrongAnimal* i = new WrongCat();
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -4,21 +4,50 @@
 
 int main()
 {
-	// Dog basic;
-	// {
-	// Dog tmp = basic;
-	// }
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	i->makeSound();
-	j->makeSound();
-	delete j;
-	delete i;
-	// Dog a;
-	// Dog b;
-	// a.setIdea(0, "Foooood");
-	// b.setIdea(0, "CAAT");
-	// a = b;
-	// std::cout << a.getIdea(0) <<std::endl;
+	{
+		Animal* zoo[10];
+		for (int i = 0; i < 5; i++)
+		{
+			zoo[i] = new Dog();
+		}
+		for (int i = 5; i < 10; i++)
+		{
+			zoo[i] = new Cat();
+		}
+		for (int i = 0; i < 10; i++)
+		{
+			(*zoo[i]).makeSound();
+		}
+		for (int i = 0; i < 10; i++)
+		{
+			delete zoo[i];
+		}
+	}
+	{
+		const Animal* meta = new Animal();
+		const Animal* j = new Dog();
+		const Animal* i = new Cat();
+		std::cout << j->getType() << " " << std::endl;
+		std::cout << i->getType() << " " << std::endl;
+		i->makeSound(); //will output the cat sound!
+		j->makeSound();
+		meta->makeSound();
+		delete meta;
+		delete j;
+		delete i;
+	}
+	{
+		Dog first;
+		Dog second(first);
+
+		first.makeSound();
+		second.makeSound();
+
+		first.setIdea(0, "alo");
+		Dog third = first;
+		first.setIdea(0, "inch");
+		std::cout << "first:: " << first.getIdea(0) <<std::endl;
+		std::cout << "third:: " << third.getIdea(0) <<std::endl;
+	}
 	return (0);
 }
